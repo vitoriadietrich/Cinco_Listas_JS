02bc147e8b0f3b13bf1dc6653f641835fe337460
Vitória de Jesus Dietrich <167101256+vitoriadietrich@users.noreply.github.com>
2026-06-16 22:19:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function alternarClasse() {
+  const paragrafo = document.getElementById("texto");
+
+  // Adiciona se não tiver, remove se já tiver
+      paragrafo.classList.toggle("ativo");
+    }
+
