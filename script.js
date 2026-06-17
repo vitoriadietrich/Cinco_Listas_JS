e24def8afad196f2baf6c7a726bc446f5a20f22a
Vitória de Jesus Dietrich <167101256+vitoriadietrich@users.noreply.github.com>
2026-06-16 22:20:28 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function alternarClasse() {
-  const paragrafo = document.getElementById("texto");
-
-  // Adiciona se não tiver, remove se já tiver
-      paragrafo.classList.toggle("ativo");
-    }
-
